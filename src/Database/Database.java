4fa3c247b116817dda6cf9ba3054453d094de813
heba <hebe10@yahoo.com>
2020-01-24 22:31:57 +0200

    con with sqlite db

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,7 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
 
-package Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+/**
+ *
+ * @author heba
+ */
+public class Database{
+    
+  
+    public static void main(String[] args) {
+        
+        
+             try{
+   Class.forName("org.sqlite.JDBC");
+    String url="jdbc:sqlite:temp.db";
+    Connection con=DriverManager.getConnection(url);
+     //Statement st=con.createStatement();
+    System.out.println("succesful");
+    
+    
+}catch(Exception ex){
 
-public class Database {
+System.out.println(" Not succesful");
+}
+        
+    }
     
 }
