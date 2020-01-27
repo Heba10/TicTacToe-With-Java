5fcfe2b06e83c04a68ae7da6f2cc2c60014bbf50
heba <hebe10@yahoo.com>
2020-01-27 03:43:50 +0200

    the first class player to connect with the table player

---
+++ src/Database/player.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author heba
+ */
+public class player {
+    Integer id;
+    String name;
+    Integer score;
+    Integer gamecountVsComputer;
+    Integer gamecountVsPlayer;
+  //  String sql="Select * from player";
+  //  pst=con.prepareStatment(sql);
+  //  rs=pst.executeQuery();
+    
+    //PreparedStatment ps=con.PrepareStatement("");
+    public player() {
+         id=0;
+   name=null;
+    score=0;
+    gamecountVsComputer=0;
+    gamecountVsPlayer=0 ; 
+        
+        
+    }
+
+    public player(ResultSet rs) throws SQLException {
+      id = rs.getInt(1);
+       name = rs.getString(2);
+        score = rs.getInt(3);
+        gamecountVsComputer = rs.getInt(4);
+        gamecountVsPlayer = rs.getInt(5);
+    }
+
+    
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+       this.id = id;
+      //getInt(1)=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Integer getGamecountVsComputer() {
+        return gamecountVsComputer;
+    }
+
+    public void setGamecountVsComputer(Integer gamecountVsComputer) {
+        this.gamecountVsComputer = gamecountVsComputer;
+    }
+
+    public Integer getGamecountVsPlayer() {
+        return gamecountVsPlayer;
+    }
+
+    public void setGamecountVsPlayer(Integer gamecountVsPlayer) {
+        this.gamecountVsPlayer = gamecountVsPlayer;
+    }
+    
+    
+    
+    
+    
+}
